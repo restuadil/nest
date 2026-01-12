b47658526e24d7cab8d840c0d3b690e9e28eca45
restuadil <restuadil09@gmail.com>
2026-01-12 15:40:54 +0700

    test(setup): setup vitest testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { Test } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { AppModule } from "../src/app.module";
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe("App E2E", () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("/GET /", async () => {
+    const res = await request(app.getHttpServer()).get("/");
+    expect(res.status).toBe(200);
   });
 });
---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import swc from "unplugin-swc";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: "./",
+    include: ["src/**/*.spec.ts", "test/**/*.e2e-spec.ts"],
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [
+    swc.vite({
+      module: { type: "es6" },
+    }),
+  ],
+});
